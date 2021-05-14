8b26c3c2ff96640edbec45295351b8f12cc9eb3f
Mukesh Soni <msoni@planview.com>
2021-05-14 21:31:41 +0530

    Added titles for each of the blog posts

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -2,7 +2,7 @@
 	const posts = import.meta.glob('./*.svx');
 	let body = [];
 	for (const path in posts) {
-		body.push(posts[path]().then(({ metadata }) => metadata));
+		body.push(posts[path]().then((post) => post));
 	}
 	/**
 	 * @type {import('@sveltejs/kit').Load}
@@ -22,20 +22,23 @@
 </script>
 
 <svelte:head>
-	<title>Blog</title>
+	<title>unstack.in blog</title>
 </svelte:head>
 
 <div class="font-sans">
 	<h1 class="font-sans break-normal text-white pt-6 pb-2 text-3xl md:text-4xl">Recent posts</h1>
 
 	<ul class="list-none">
-		{#each posts as { title, slug }}
+		{#each posts as post}
 			<li class="list-none">
-				<a class="text-blue-500 space-y-3" rel="prefetch" href="blog/{slug}">
+				<a class="text-blue-500 space-y-3" rel="prefetch" href="blog/{post.metadata.slug}">
 					<h2>
-						{title}
+						{post.metadata.title}
 					</h2>
 				</a>
+				<div>
+					{post.metadata.date}
+				</div>
 			</li>
 		{/each}
 	</ul>
