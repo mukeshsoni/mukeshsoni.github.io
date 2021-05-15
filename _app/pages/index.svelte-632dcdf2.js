24d44a23756b7cc303e63e3af186e6064262274e
Mukesh Soni <msoni@planview.com>
2021-05-15 16:30:33 +0530

    Updates

---
+++ _app/pages/index.svelte-632dcdf2.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as r}from"../chunks/vendor-ce80dba4.js";function e(){return{redirect:"blog",status:301}}export default class extends s{constructor(s){super(),t(this,s,null,null,r,{})}}export{e as load};
