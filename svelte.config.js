259479083428bb14712c0d8cd93efc5b1f09afd2
Mukesh Soni <mukeshsoni@gmail.com>
2023-02-27 10:58:00 +0530

    use adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 const mdsvexExtensions = ['.md'];
 
